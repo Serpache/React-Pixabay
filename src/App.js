4776b415a4fb64d6b4b29982f1e3a6de639853b0
Pache <serpache@ucm.es>
2021-01-15 20:34:53 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Formulario from './components/Formulario'
 import ListadoImagenes from './components/ListadoImagenes'
@@ -7,37 +7,73 @@ function App() {
 
   //states
   const [busqueda, guardarBusqueda] = useState("");
-  const [imagenes, guardarImagenes] = useState([])
+  const [imagenes, guardarImagenes] = useState([]);
+  const [paginaActual, guardarPaginaActual] = useState(1);
+  const [totalPaginas, guardarTotalPaginas] = useState(1);
 
   useEffect(() => {
     const consultarAPI = async () => {
-      if(busqueda === "") return;
+      if (busqueda === "") return;
 
-      const imagenesPorPagina=30;
+      const imagenesPorPagina = 30;
       //Key privada de pixabay
-      const key ="19901694-ed3a53ae97a3d543183ad9483";
-      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
-  
+      const key = "";
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaActual}`;
+
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
 
       guardarImagenes(resultado.hits);
+
+      //calcular total páginas
+      const totalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina);
+      guardarTotalPaginas(totalPaginas);
+
+      //mover vista hacia arriba
+      const jumbotron = document.querySelector(".jumbotron");
+      jumbotron.scrollIntoView({behavior: "smooth"});
     }
     consultarAPI();
-  }, [busqueda])
+  }, [busqueda, paginaActual]);
+
+  //Paginado
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaActual - 1;
+    if (nuevaPaginaActual === 0) return;
+    guardarPaginaActual(nuevaPaginaActual);
+  }
+  const paginaSiguiente = () => {
+    const nuevaPaginaActual = paginaActual + 1;
+    if (nuevaPaginaActual > totalPaginas) return;
+    guardarPaginaActual(nuevaPaginaActual);
+  }
 
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de imágenes</p>
-        <Formulario 
+        <Formulario
           guardarBusqueda={guardarBusqueda}
         />
       </div>
       <div className="row justify-content-center">
-        <ListadoImagenes 
+        <ListadoImagenes
           imagenes={imagenes}
         />
+        {(paginaActual === 1) ? null : (
+          <button
+            type="button"
+            className="bbtn btn-info mr-1"
+            onClick={paginaAnterior}
+          >&laquo; Anterior</button>
+        )}
+        {(paginaActual === totalPaginas) ? null : (
+          <button
+            type="button"
+            className="bbtn btn-info"
+            onClick={paginaSiguiente}
+          >Siguiente &raquo;</button>
+        )}
       </div>
     </div>
   );
