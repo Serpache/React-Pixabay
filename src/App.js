9196086b4aa2ce1bc74c5fc49e4fc8feac87707e
Pache <serpache@ucm.es>
2021-01-15 18:34:58 +0100

    search and cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import Formulario from './components/Formulario'
+import ListadoImagenes from './components/ListadoImagenes'
 
 function App() {
 
   //states
   const [busqueda, guardarBusqueda] = useState("");
+  const [imagenes, guardarImagenes] = useState([])
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if(busqueda === "") return;
+
+      const imagenesPorPagina=30;
+      //Key privada de pixabay
+      const key ="19901694-ed3a53ae97a3d543183ad9483";
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+  
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarImagenes(resultado.hits);
+    }
+    consultarAPI();
+  }, [busqueda])
 
   return (
     <div className="container">
@@ -13,6 +32,11 @@ function App() {
         <p className="lead text-center">Buscador de imágenes</p>
         <Formulario 
           guardarBusqueda={guardarBusqueda}
+        />
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes 
+          imagenes={imagenes}
         />
       </div>
     </div>
---
+++ src/components/ListadoImagenes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Imagen from './Imagen'
+
+const ListadoImagenes = ({imagenes}) => {
+    return ( 
+        <div className="col12 p5 row">
+            {imagenes.map(imagen => (
+                <Imagen 
+                    key={imagen.id}
+                    imagen={imagen}
+                />
+            ))}
+        </div>
+     );
+}
+ 
+export default ListadoImagenes;
