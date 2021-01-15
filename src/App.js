4f9b627e943058801ab5344de7652b73236270be
Pache <serpache@ucm.es>
2021-01-15 18:02:38 +0100

    error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import Formulario from './components/Formulario'
 
 function App() {
+
+  //states
+  const [busqueda, guardarBusqueda] = useState("");
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de imágenes</p>
         <Formulario 
-        
+          guardarBusqueda={guardarBusqueda}
         />
       </div>
     </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
+import Error from './Error'
 
-const Formulario = () => {
+const Formulario = ({guardarBusqueda}) => {
+
+    //states
+    const [termino,guardarTermino] = useState("");
+    const [error,guardarError] = useState(false);
+
+    const buscarImagenes = e => {
+        e.preventDefault();
+
+        //Validar
+        if(termino.trim() === ""){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        //Enviar a App
+        guardarBusqueda(termino);
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={buscarImagenes}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input 
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Busca una imagen, ejemplo: futbol o café"
+                        onChange={e => {guardarTermino(e.target.value)}}
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -20,6 +43,8 @@ const Formulario = () => {
                     />
                 </div>
             </div>
+
+            {error ? <Error mensaje="Introducir un término de busqueda"/> : null}
         </form>
      );
 }
